e28be595493a88dc8e0e3dd8224ab097756bf992
Elliot Marsden <elliot.marsden@gmail.com>
2016-04-23 17:53:38 +0100

    Add proof-of-concept of using global variables in Cython

--- test/cy_test.pyx
+++ test/cy_test.pyx
@@ -3,10 +3,19 @@ cdef extern from "main.h":
     void main_init(int ac, char *av[])
     int main_body()
 
+cdef extern from "main.h":
+    int shell_enabled_p
+
+
 
 def test(fname):
     cdef char **b = ["cytex", "-ini", fname]
     cdef int i = 3
     main_init(i, b)
     cdef int ret = main_body()
+    set_bool()
     return ret
+
+
+def set_bool():
+    shell_enabled_p = 1
