8cd6a21806a1716959140658a781da547066f3d2
Elliot Marsden <elliot.marsden@gmail.com>
2016-04-25 10:19:11 +0100

    Remove unnecessary extern keyword on function
    Pretty sure it does not do anything for functions, they are
    extern regardless.

--- cpdfetex/main.h
+++ cpdfetex/main.h
@@ -9,6 +9,6 @@ EXTERN int          main (int ac,  string *av);
 EXTERN void         get_date_and_time (integer *,integer *,integer *, integer *);
 EXTERN integer      getrandomseed(void);
 EXTERN void         setup_bound_variable (integer * var,  const_string var_name,  integer dflt);
-extern int main_body (void);
+int main_body (void);
 
 #endif /* not TEX_MAIN_H */
