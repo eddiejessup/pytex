fe6f5b86d8058eef8698f87b903f17baaa9e7b68
Elliot Marsden <elliot.marsden@gmail.com>
2016-05-08 13:58:08 +0200

    Fix typos in comment, 'MODE_FIELD' to 'mode'

--- cpdfetex/control.c
+++ cpdfetex/control.c
@@ -163,15 +163,15 @@ void handle_easy_cases(void) {
 	/* module 1207 */
 	/* Many of the actions related to box-making are triggered by the appearance
 	 * of braces in the input. For example, when the user says `\.{\\hbox}
-	 * \.{to} \.{100pt\{$\langle\,\hbox{hlist}\,\rangle$\}}' in vertical MODE_FIELD,
+	 * \.{to} \.{100pt\{$\langle\,\hbox{hlist}\,\rangle$\}}' in vertical mode,
 	 * the information about the box size (100pt, |exactly|) is put onto |save_stack|
 	 * with a level boundary word just above it, and |cur_group:=adjusted_hbox_group|;
-	 * \TeX\ enters restricted horizontal MODE_FIELD to process the hlist. The right
+	 * \TeX\ enters restricted horizontal mode to process the hlist. The right
 	 * brace eventually causes |save_stack| to be restored to its former state,
 	 * at which time the information about the box size (100pt, |exactly|) is
 	 * available once again; a box is packaged and we leave restricted horizontal
-	 * MODE_FIELD, appending the new box to the current list of the enclosing MODE_FIELD
-	 * (in this case to the current list of vertical MODE_FIELD), followed by any
+	 * mode, appending the new box to the current list of the enclosing mode
+	 * (in this case to the current list of vertical mode), followed by any
 	 * vertical adjustments that were removed from the box by |hpack|.
 	 * 
 	 * The next few sections of the program are therefore concerned with the
@@ -180,7 +180,7 @@ void handle_easy_cases(void) {
 	/* module 1208 */
 	/* If a left brace occurs in the middle of a page or paragraph, it simply
 	 * introduces a new level of grouping, and the matching right brace will not have
-	 * such a drastic effect. Such grouping affects neither the MODE_FIELD nor the
+	 * such a drastic effect. Such grouping affects neither the mode nor the
 	 * current list.
 	 */
   case NON_MATH (left_brace):
